a3e8f297605b58ef0f506675db4d9a635e6396dc
Granthik Ghediya <granthikghediya@gmail.com>
2025-01-27 15:22:25 +0530

    Update

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import ExpenseItem from './components/ExpenseItem';
 
 function App() {
+  const expenses = [
+    { title: 'Cars', amount: 25666, date: new Date(2025, 0, 27) },
+    { title: 'Shoes', amount: 56866, date: new Date(2025, 0, 28) },
+    { title: 'Pc', amount: 20566, date: new Date(2025, 0, 29) },
+    { title: 'Phones', amount: 65366, date: new Date(2025, 0, 30) },
+  ];
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>Hello !!!</h2>
+        <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date} />
+        <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date} />
+        <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date} />
+        <ExpenseItem title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date} />
       </header>
     </div>
   );
